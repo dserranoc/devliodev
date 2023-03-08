f9bb56ebc0ae5d30b2d12d88bf7125b0b28ed9b7
Daniel Serrano Cobos <d.serranoc@outlook.com>
2023-03-08 19:24:53 +0100

    feat(packages/api): Create middleware to get user from request

---
+++ packages/api/src/middlewares/getUserFromReq.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from '../utils/jwt'
+
+const getUserFromReq = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const authorization = req.headers.authorization
+  if (authorization === undefined || authorization === null) {
+    return next()
+  }
+
+  const accessToken = authorization.replace(/^Bearer\s/, '')
+
+  if (accessToken === undefined || accessToken === null || accessToken === '') {
+    return next()
+  }
+
+  const { decoded } = jwt.verify(accessToken)
+
+  if (decoded !== null) {
+    res.locals.user = decoded
+    return next()
+  }
+
+  return next()
+}
+export default getUserFromReq
