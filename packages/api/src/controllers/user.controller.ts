bc1cc531ce65b86d6c03adaabe11f1577ca1c157
Daniel Serrano Cobos <d.serranoc@outlook.com>
2023-03-14 09:45:17 +0100

    feat(packages/api): Create delete user action

--- packages/api/src/controllers/user.controller.ts
+++ packages/api/src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
-import { CreateUserInput, createUserSchema, LoginUserInput, UpdateUserInput } from '../schemas/user.schema'
+import { CreateUserInput, createUserSchema, DeleteUserInput, LoginUserInput, UpdateUserInput } from '../schemas/user.schema'
 import UserService from '../services/user.service'
 import { StandardResponse } from '../types/types.d'
 import jwt from '../utils/jwt'
@@ -49,6 +49,22 @@ const userController = {
     const user = await UserService.findAndUpdate({ _id: userId }, req.body, { new: true })
 
     return res.status(200).json({ success: true, content: user })
+  },
+  delete: async (req: Request<DeleteUserInput['params']>, res: Response<StandardResponse, {}>, _next: NextFunction) => {
+    const userId = res.locals.user._id
+
+    if (userId === null) {
+      return res.status(401).json({
+        success: false,
+        error: {
+          message: 'Invalid credentials'
+        }
+      })
+    }
+
+    await UserService.delete({ _id: userId })
+
+    return res.status(200).json({ success: true, message: 'User deleted successfully!' })
   }
 }
 
