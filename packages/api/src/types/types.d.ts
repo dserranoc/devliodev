12128ba97f76e4dab1d010d47dc1221f23ac7c10
Daniel Serrano Cobos <d.serranoc@outlook.com>
2023-03-14 09:43:41 +0100

    feat(packages/api): Add user to Locals express interface

--- packages/api/src/types/types.d.ts
+++ packages/api/src/types/types.d.ts
@@ -2,4 +2,16 @@ export interface StandardResponse {
   success: boolean
   content?: any
   error?: any
+  message?: string
+}
+
+declare global {
+  namespace Express {
+    interface Locals {
+      user: {
+        email: string
+        _id: string
+      }
+    }
+  }
 }
