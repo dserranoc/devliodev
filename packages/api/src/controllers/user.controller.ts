6bfa08dd607df4d0cfd570415601df0f3199dd69
Daniel Serrano Cobos <d.serranoc@outlook.com>
2023-03-14 09:44:49 +0100

    feat(packages/api): Create update user action

--- packages/api/src/controllers/user.controller.ts
+++ packages/api/src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
-import { CreateUserInput, createUserSchema, LoginUserInput } from '../schemas/user.schema'
+import { CreateUserInput, createUserSchema, LoginUserInput, UpdateUserInput } from '../schemas/user.schema'
 import UserService from '../services/user.service'
 import { StandardResponse } from '../types/types.d'
 import jwt from '../utils/jwt'
@@ -33,6 +33,22 @@ const userController = {
         accessToken
       }
     })
+  },
+  update: async (req: Request<UpdateUserInput['body']>, res: Response<StandardResponse, {}>, _next: NextFunction) => {
+    const userId = res.locals.user._id
+
+    if (userId === null) {
+      return res.status(401).json({
+        success: false,
+        error: {
+          message: 'Invalid credentials'
+        }
+      })
+    }
+
+    const user = await UserService.findAndUpdate({ _id: userId }, req.body, { new: true })
+
+    return res.status(200).json({ success: true, content: user })
   }
 }
 
